36269e4f72d9262b1fd86b0b99590e35b7b3c013
Scott King <kingsc850@gmail.com>
2016-08-04 23:19:10 -0400

    Bisection methods works

--- bisection.rs
+++ bisection.rs
@@ -1,20 +1,18 @@
-use std::f32;
-
 fn main() {
-    println!("{}", bisection(|x| { x*x*x + 3*x - 1 }, 0, 1));                 // Going to return 1*1*1 + 3*1 - 1 = 3
+    println!("{}", bisection(|x| { x*x*x + x - 1_f32 }, 0, 1));
 }
 
-fn bisection<F: FnOnce(f32) -> f32>(f: F, x: i32, y: i32) -> i32 {
+fn bisection<F: Fn(f32) -> f32>(f: F, x: i32, y: i32) -> f32 {
     let mut a = x as f32;
     let mut b = y as f32;
-    let mut r = 0 as f32;
+    let mut r = 0_f32;
 
-    while (b - a).abs() >= 1e-4f32 {
-        r = (a + b) / 2;
+    while (b - a).abs() >= 1e-4 {
+        r = (a + b) / 2_f32;
 
-        if f(a) == 0f32 {
+        if f(a) == 0_f32 {
             break;
-        } else if (f(r) * f(a)) < 0f32 {
+        } else if f(r) * f(a) < 0_f32 {
             b = r;
         } else {
             a = r;
